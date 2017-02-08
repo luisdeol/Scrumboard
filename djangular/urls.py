ad6620b8e8ac6e85cf5a3f98ebe4e2370a4ebc4d
luisfelipekai <luisfelipekai@gmail.com>
2017-02-08 20:28:16 -0300

    Implement DRF Serializers and Class-Views for List and Card.

--- djangular/urls.py
+++ djangular/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'djangular.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^scrumboard/', include('scrumboard.urls'))
 ]
