92e613bfd5cad3e5dea8bc31ef5d7e32d599973a
luisfelipekai <luisfelipekai@gmail.com>
2017-02-11 10:12:43 -0300

    Create card directive, card html, scrumboard css and implement POST, PUT and DELETE from Client-side.

--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -7,13 +7,20 @@
 
             $scope.add = function(list, title){
                 var card = {
-                    title: title
+                    title: title,
+                    list: list.id
                 };
-                list.cards.push(card);
+                $http.post('/scrumboard/cards/', card)
+                    .then(function(response){
+                        list.cards.push(response.data);
+                    },
+                    function(){
+                        alert("Something went wrong!");
+                    });
             };
 
             $scope.data = [];
-            $http.get('/scrumboard/lists')
+            $http.get('/scrumboard/lists/')
                 .then(function(response){
                     $scope.data = response.data;
                 });
