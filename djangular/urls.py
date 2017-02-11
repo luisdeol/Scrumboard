b30afbdfd0dc85f1b282c35d3de21c89b7c1fa9d
luisfelipekai <luisfelipekai@gmail.com>
2017-02-11 08:49:38 -0300

    Implement POST, PUT, DELETE and GET using ViewSets.

--- djangular/urls.py
+++ djangular/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^scrumboard/', include('scrumboard.urls'))
+    url(r'^scrumboard/', include('scrumboard.urls')),
+    url(r'^$', TemplateView.as_view(template_name="scrumboard/home.html"))
 ]
